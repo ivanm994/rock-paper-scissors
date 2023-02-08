0d2356539661942577567be7d028c824d60b050a
ivanm994 <ivan.mijailovic994@gmail.com>
2023-02-08 18:29:41 +0100

    Make function that uses switch to compare strings and decide winner

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getComputerChoice()
 {
-   let rpsArray = ["Rock", "Paper", "Scissors"];
+   let rpsArray = ["rock", "paper", "scissors"];
    
    let randomIndex = Math.floor(Math.random() * rpsArray.length);
 
@@ -8,4 +8,33 @@ function getComputerChoice()
    return play;
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice()
+{
+   let choice = prompt("Please write your weapon: ");
+   return choice.toLowerCase();
+}
+
+function playRound (playerSelection, computerSelection)
+{
+   let winner;
+   switch (playerSelection + computerSelection)
+   {     
+      case "rockscissors":
+      case "scissorspaper":
+      case "paperrock":
+         return winner = "YOU WIN"
+         break
+      case "scissorsrock":
+      case "paperscissors":
+      case "rockpaper":
+         return winner = "YOU LOSE"
+         break
+      case "rockrock":
+      case "scissorsscissors":
+      case "paperpaper":
+         return winner = "DRAW"
+         break
+   }
+}
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
